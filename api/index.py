d922efec8e0fa51378c74384b473f7491b419fd3
ghepa2003 <lorenzo.ghessi@mail.polimi.it>
2026-06-19 18:07:39 +0200

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,3 +1,7 @@
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 from fastapi.middleware.cors import CORSMiddleware
